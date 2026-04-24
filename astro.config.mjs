45f1f02bb3e200802c77bbfb0a217d5e28c277d5
Yvonne Tang <ytang2@stanford.edu>
2026-04-24 12:08:00 -0700

    Clean up fonts config, add stanford fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -7,4 +7,34 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  fonts: [
+    {
+      name: "Source Sans 3",
+      cssVariable: "--font-sans",
+      provider: fontProviders.google(),
+      weights: [400, 600] ,
+    },
+    {
+      name: "Source Serif 4",
+      cssVariable: "--font-serif",
+      provider: fontProviders.fontsource(),
+      styles: ["normal"],
+    },
+    {
+      name: "Stanford",
+      cssVariable: "--font-stanford",
+      provider: fontProviders.local(),
+      options: {
+        // Weight and style are not specified so Astro
+        // will try to infer them for each variant
+        variants: [
+          {
+            src: [
+              "./src/assets/fonts/stanford.woff2",
+            ]
+          },
+        ]
+      }
+    }
+  ],
 });
