822bb87edc6ed497658dccc459bedb35db3c50f4
Yvonne Tang <ytang2@stanford.edu>
2026-04-27 17:34:45 -0700

    fix screenshot and animation

--- src/data/screenshotCells.ts
+++ src/data/screenshotCells.ts
@@ -1,15 +1,23 @@
 import type { ScreenshotCell } from "../ScreenshotGrid";
+import screenshotBioengineering from "../images/Solutions images/Bioengineering.jpg";
+import screenshotEngineeringMain from "../images/Solutions images/Eng Main Site.jpg";
+import screenshotIcme from "../images/Solutions images/ICME.jpg";
+import screenshotMechanicalEngineering from "../images/Solutions images/Mechanical Eng.jpg";
+import screenshotChemh from "../images/Stanford Sites images/Chemh 1.png";
+import screenshotCSharp from "../images/Stanford Sites images/C-Sharp.jpg";
+import screenshotResearchHub from "../images/Stanford Sites images/Research Hub.jpg";
+import screenshotTechResources from "../images/Stanford Sites images/Tech Resources.jpg";
 
 export const stanfordSitesCells: ScreenshotCell[] = [
-  { id: "c1", src: "/images/Stanford Sites images/Chemh 1.png", alt: "" },
-  { id: "c2", src: "/images/Stanford Sites images/Research Hub.jpg", alt: "" },
-  { id: "c3", src: "/images/Stanford Sites images/C-Sharp.jpg", alt: "" },
-  { id: "c4", src: "/images/Stanford Sites images/Tech Resources.jpg", alt: "" },
+  { id: "c1", src: screenshotChemh.src, alt: "Chem h" },
+  { id: "c2", src: screenshotResearchHub.src, alt: "" },
+  { id: "c3", src: screenshotCSharp.src, alt: "" },
+  { id: "c4", src: screenshotTechResources.src, alt: "" },
 ];
 
 export const solutionsCells: ScreenshotCell[] = [
-  { id: "c1", src: "/images/Solutions images/Eng Main Site.jpg", alt: "" },
-  { id: "c2", src: "/images/Solutions images/ICME.jpg", alt: "" },
-  { id: "c3", src: "/images/Solutions images/Bioengineering.jpg", alt: "" },
-  { id: "c4", src: "/images/Solutions images/Mechanical Eng.jpg", alt: "" },
+  { id: "c1", src: screenshotEngineeringMain.src, alt: "" },
+  { id: "c2", src: screenshotIcme.src, alt: "" },
+  { id: "c3", src: screenshotBioengineering.src, alt: "" },
+  { id: "c4", src: screenshotMechanicalEngineering.src, alt: "" },
 ];
