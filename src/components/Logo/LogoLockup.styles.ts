4ddad584743ab161b4736bc90b46a5f4f14233e2
rebeccahongsf <rebeccahongsf@gmail.com>
2026-05-04 15:52:17 -0700

    update to horizontal lockup style on mobile

--- src/components/Logo/LogoLockup.styles.ts
+++ src/components/Logo/LogoLockup.styles.ts
@@ -1,8 +1,8 @@
 export const root = 'no-underline inline-block font-normal';
-export const contentWrapper = 'flex-col sm:flex-row items-start sm:items-end sm:gap-10';
-export const logo = 'text-19 sm:type-3 mt-[0.27em]';
+export const contentWrapper = 'flex-row items-end gap-10';
+export const logo = 'type-3 mt-[0.27em]';
 export const bar = 'hidden sm:block w-1 min-h-1em h-full mx-03em';
-export const text = 'sm:pl-12 sm:border-l text-15 sm:type-0 mt-03em -ml-01em sm:ml-0';
+export const text = 'pl-12 border-l type-0 mt-03em -ml-01em sm:ml-0';
 export const line2 = 'text-15 sm:text-18 italic';
 export const textColors = {
   default: 'text-black-90 border-black-40',
