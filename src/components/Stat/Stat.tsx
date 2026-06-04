1b8f4e4ba7824835e8047da8bcc9a661783cae0a
Yvonne Tang <ytang2@stanford.edu>
2026-06-04 00:18:55 -0700

    animate stats

--- src/components/Stat/Stat.tsx
+++ src/components/Stat/Stat.tsx
@@ -1,13 +1,30 @@
+import { useRef } from 'react';
+import NumberFlow from '@number-flow/react';
+import { useInView } from 'motion/react';
+
 type StatProps = {
-  stat: string;
+  stat: number;
+  suffix?: string;
   label: string;
 }
 
-export const Stat = ({ stat, label }: StatProps) => {
+export const Stat = ({ stat, suffix, label }: StatProps) => {
+  const counterRef = useRef<HTMLDivElement>(null);
+  const isCounterInView = useInView(counterRef, { once: true, amount: 0.1 });
+
   return (
-    <li className="flex flex-col mb-0">
-      <span className="font-serif fluid-type-7 font-normal leading-snug text-dark-primary">{stat}</span>
+    <div className="flex flex-col mb-0">
+      <div
+        ref={counterRef}
+        className="text-center md:text-left inline-block font-serif fluid-type-8 font-normal leading-snug text-dark-primary"
+      >
+        <NumberFlow
+          value={isCounterInView ? stat : 0}
+          format={{ useGrouping: false }} // Disable commas in numbers
+          suffix={suffix}
+        />
+      </div>
       <span className="card-paragraph leading-snug text-black-30">{label}</span>
-    </li>
+    </div>
   );
 };
