ee680e37c8631da0e7382392710df2ce6649cdc2
Yvonne Tang <42749717+yvonnetangsu@users.noreply.github.com>
2026-08-20 14:04:37 -0700

    SWSWEB-105 | A11y fixes (#32)
    * Update the repository hydration guidance in AGENTS.md to reflect the actual recommended strategy
    
    * renders inert={!drawerOpen} in the initial HTML and removes the redundant post-hydration effect
    
    * replace hover with hocus-visible for links/buttons in the header and footer
    
    * remove duplicate sitemap link in HTML head; copy fixup in CollaborationSection
    
    * replaced spacer with CSS grid property in local footer; simplified the width lookup in Container
    
    * add footer landmark around local and global footer; improve nav usage in Global Footer
    
    * Remove role="presentation" on the video and add aria-hidden="true" instead

--- src/components/GlobalFooter/GlobalFooter.tsx
+++ src/components/GlobalFooter/GlobalFooter.tsx
@@ -9,108 +9,112 @@ export const GlobalFooter = () => (
         <StanfordLogo isLink tabIndex={-1} aria-hidden className={styles.logo} type="stacked" color="white" />
       </div>
       <div className={styles.contentWrapper}>
-        <nav aria-label="global footer menu" className={styles.menusWrapper}>
-          <ul className={styles.stanfordMenu}>
-            <li className={styles.listItem}>
-              <a
-                href="https://www.stanford.edu"
-                rel="nofollow"
-                className={styles.link}
-              >
-                Stanford Home
-              </a>
-            </li>
-            <li className={styles.listItem}>
-              <a
-                href="https://visit.stanford.edu/basics"
-                rel="nofollow"
-                className={styles.link}
-              >
-                Maps &amp; Directions
-              </a>
-            </li>
-            <li className={styles.listItem}>
-              <a
-                href="https://www.stanford.edu/search/"
-                rel="nofollow"
-                className={styles.link}
-              >
-                Search Stanford
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://emergency.stanford.edu"
-                rel="nofollow"
-                className={styles.link}
-              >
-                Emergency Info
-              </a>
-            </li>
-          </ul>
-          <ul className={styles.legalMenu}>
-            <li className={styles.listItem}>
-              <a
-                href="https://www.stanford.edu/terms/"
-                rel="nofollow"
-                title="Terms of use for sites"
-                className={styles.link}
-              >
-                Terms of Use
-              </a>
-            </li>
-            <li className={styles.listItem}>
-              <a
-                href="https://www.stanford.edu/privacy/"
-                rel="nofollow"
-                title="Privacy and cookie policy"
-                className={styles.link}
-              >
-                Privacy
-              </a>
-            </li>
-            <li className={styles.listItem}>
-              <a
-                href="https://uit.stanford.edu/security/copyright-infringement"
-                rel="nofollow"
-                title="Report alleged copyright infringement"
-                className={styles.link}
-              >
-                Copyright
-              </a>
-            </li>
-            <li className={styles.listItem}>
-              <a
-                href="https://adminguide.stanford.edu/chapter-1/subchapter-5/policy-1-5-4"
-                rel="nofollow"
-                title="Ownership and use of Stanford trademarks and images"
-                className={styles.link}
-              >
-                Trademarks
-              </a>
-            </li>
-            <li className={styles.listItem}>
-              <a
-                href="https://non-discrimination.stanford.edu/"
-                rel="nofollow"
-                title="Non-discrimination policy"
-                className={styles.link}
-              >
-                Non-Discrimination
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.stanford.edu/accessibility"
-                rel="nofollow"
-                title="Report web accessibility issues"
-                className={styles.link}
-              >
-                Accessibility
-              </a>
-            </li>
-          </ul>
-        </nav>
+        <div className={styles.menusWrapper}>
+          <nav aria-label="Stanford University important links">
+            <ul className={styles.stanfordMenu}>
+              <li className={styles.listItem}>
+                <a
+                  href="https://www.stanford.edu"
+                  rel="nofollow"
+                  className={styles.link}
+                >
+                  Stanford Home
+                </a>
+              </li>
+              <li className={styles.listItem}>
+                <a
+                  href="https://visit.stanford.edu/basics"
+                  rel="nofollow"
+                  className={styles.link}
+                >
+                  Maps &amp; Directions
+                </a>
+              </li>
+              <li className={styles.listItem}>
+                <a
+                  href="https://www.stanford.edu/search/"
+                  rel="nofollow"
+                  className={styles.link}
+                >
+                  Search Stanford
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://emergency.stanford.edu"
+                  rel="nofollow"
+                  className={styles.link}
+                >
+                  Emergency Info
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <nav aria-label="Stanford University legal and policies">
+            <ul className={styles.legalMenu}>
+              <li className={styles.listItem}>
+                <a
+                  href="https://www.stanford.edu/terms/"
+                  rel="nofollow"
+                  title="Terms of use for sites"
+                  className={styles.link}
+                >
+                  Terms of Use
+                </a>
+              </li>
+              <li className={styles.listItem}>
+                <a
+                  href="https://www.stanford.edu/privacy/"
+                  rel="nofollow"
+                  title="Privacy and cookie policy"
+                  className={styles.link}
+                >
+                  Privacy
+                </a>
+              </li>
+              <li className={styles.listItem}>
+                <a
+                  href="https://uit.stanford.edu/security/copyright-infringement"
+                  rel="nofollow"
+                  title="Report alleged copyright infringement"
+                  className={styles.link}
+                >
+                  Copyright
+                </a>
+              </li>
+              <li className={styles.listItem}>
+                <a
+                  href="https://adminguide.stanford.edu/chapter-1/subchapter-5/policy-1-5-4"
+                  rel="nofollow"
+                  title="Ownership and use of Stanford trademarks and images"
+                  className={styles.link}
+                >
+                  Trademarks
+                </a>
+              </li>
+              <li className={styles.listItem}>
+                <a
+                  href="https://non-discrimination.stanford.edu/"
+                  rel="nofollow"
+                  title="Non-discrimination policy"
+                  className={styles.link}
+                >
+                  Non-Discrimination
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.stanford.edu/accessibility"
+                  rel="nofollow"
+                  title="Report web accessibility issues"
+                  className={styles.link}
+                >
+                  Accessibility
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
         <div className={styles.copyright}>
           <span className={styles.copyrightText}>&copy; Stanford University.</span>
           <span className={styles.copyrightText}>&nbsp; Stanford, California 94305.</span>
