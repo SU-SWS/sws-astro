f561ecb91586d8cd33901b5bee66f635ac470ddb
Yvonne Tang <ytang2@stanford.edu>
2026-09-21 03:16:41 -0700

    content update with final copy

--- src/components/FooterLocal.tsx
+++ src/components/FooterLocal.tsx
@@ -10,11 +10,14 @@ export function FooterLocal({
 }: FooterLocalProps) {
   return (
     <section className="cc bg-fill-secondary rs-py-6">
-      <div className="grid md:grid-cols-2 md:gap-40 rs-pb-4">
-        <div className="max-w-4xl flex-1 basis-md md:col-start-2">
-          <h2 className="rs-mb-2 font-serif type-2 font-normal leading-display">
-            We’d love to hear about what you’re working on.
+      <div className="grid lg:grid-cols-2 lg:gap-40 rs-pb-4">
+        <div className="max-w-600 flex-1 basis-md lg:col-start-2">
+          <h2 className="mb-10 font-serif type-2 font-normal leading-display">
+            We’d love to hear from you.
           </h2>
+          <p className="card-paragraph rs-mb-0">
+            Whether you’re just getting started, planning a site refresh, or expanding your digital presence, our team is here to guide you through the process, every step of the way.
+          </p>
           <CtaLink variant="button-light" id="local-footer-connect-cta" href="/contact/">
             Connect with us
           </CtaLink>
